9fe2f877f2055cc26ea7646bfcde0954aec08519
rrouwelng <rouwelngacha@gmail.com>
2026-05-24 17:31:01 +0300

    Feat: Added the router logic to the server and handlers. Feat: Removed commented code

--- main.go
+++ main.go
@@ -2,12 +2,13 @@
 package main
 
 import (
-	//"fmt"
+	"fmt"
 	"log"
 	"net/http"
 	"restedancestor/database"
 	"restedancestor/handlers"
-	//"github.com/julienschmidt/httprouter"
+
+	"github.com/julienschmidt/httprouter"
 )
 
 func main() {
@@ -15,37 +16,20 @@ func main() {
 	database.Database("database/database.db")
 	defer database.DB.Close()
 	handlers.DB = database.DB
-	// 2. Map routes to handlers from handlers.go
-	http.HandleFunc("/all", handlers.GetQuotesHandler)
-	http.HandleFunc("/api/quotes/{id}", handlers.GetQuoteByIDHandler)
-	http.HandleFunc("/", handlers.Random)
+	//Define router to handle all routes
+
+	router := httprouter.New()
+	//list of routes
+	router.GET("/all", handlers.GetQuotesHandler)
+	router.GET("/", handlers.Random)
+	router.GET("/quotes/:id", handlers.GetQuoteByIDHandler)
+
+	//Server initialization message
+	fmt.Println("Welcome to restedancestor, the API is running in a maddening fashion!")
+	fmt.Println("The Ancestor is waiting and listening on localhost:8080")
 
 	log.Println("Server starting on :8080...")
-	if err := http.ListenAndServe(":8080", nil); err != nil {
+	if err := http.ListenAndServe(":8080", router); err != nil {
 		log.Fatal(err)
 	}
 }
-
-//func main() {
-// initiates router
-//	router := httprouter.New()
-
-// lists routes
-//	router.GET("/random", handlers.Random)
-//	router.GET("/all", handlers.All)
-//	router.GET("/senile", handlers.Senile)
-//	router.GET("/search/:word", handlers.Search)
-//	router.GET("/top", handlers.Top)
-//commented the error handlers length
-//router.GET("/length/:len", handlers.Length)
-//uuid routes
-//	router.GET("/uuid/:uuid/find", handlers.Find)
-//	router.POST("/uuid/:uuid/like", handlers.Like)
-//	router.POST("/uuid/:uuid/dislike", handlers.Dislike)
-//	fmt.Println("Welcome to restedancestor, the API is running in a maddening fashion!")
-//	fmt.Println("The Ancestor is waiting and listening on localhost:8080")
-//	err := http.ListenAndServe(":8080", router)
-//	if err != nil {
-//		log.Fatal(err)
-//	}
-//}
